ac13f85a882b73a097af55d05cedd243e5e19a8c
Tyshkan4ik <001vat@gmail.com>
2023-03-14 13:15:19 +0300

    created a project

---
+++ Weather forecast v2.0/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather forecast v2.0
+//
+//  Created by Виталий Троицкий on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
+
